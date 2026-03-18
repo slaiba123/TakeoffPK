8a4bba7041378b02afa269addc44a401a01e125c
slaiba123 <139244200+slaiba123@users.noreply.github.com>
2026-03-18 14:31:28 +0500

    updated app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 from src.prompt import system_prompt
 import os
-
+#comment added to test ci/cd
 app = Flask(__name__)
 
 load_dotenv()
